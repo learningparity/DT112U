cec54454fedbc4534d91c69a034e0e46133d860f
AA <73504371+learningparity@users.noreply.github.com>
2020-11-03 03:57:30 +0100

    Andreas (#3)
    * Update Value Iteration Solution.py
    
    Working version, does not work properly.
    
    * Finally solved the value iteration

--- DynamicProgramming/Value Iteration Solution.py
+++ DynamicProgramming/Value Iteration Solution.py
@@ -1,25 +1,41 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[3]:
-
-
 import numpy as np
-import pprint
+#import pprint
 import sys
 if "../" not in sys.path:
   sys.path.append("../") 
-from lib.envs.gridworld import GridworldEnv
 
+#from lib.envs.gridworld import GridworldEnv
+#env = GridworldEnv()
 
-# In[4]:
+from lib.envs.cliff_walking import CliffWalkingEnv
+env = CliffWalkingEnv()
+#pp = pprint.PrettyPrinter(indent=2)
 
+def lookahead(env, state, V, discount_factor):
+    """
+    The greedy policy takes the action that looks best in the short term,
+    after one step of lookahead—according to V.
+    Args:
+        env: The OpenAI envrionment.
+        state: The state to consider (int)
+        V: The value to use as an estimator, Vector of length env.nS
+    Returns:
+        A vector of length env.nA containing the expected value of each action.
+    """
+    action_values = np.zeros(env.nA)
+    for a in range(env.nA):
+        for prob, next_state, reward, done in env.P[state][a]:
+            #action_values[a] += prob * (reward + discount_factor * V[next_state])
+            # How to deal with reaching terminal states?
+            # Is this correct?
+            if done:
+                action_values[a] += prob * reward
+            else:
+                action_values[a] += prob * (reward + discount_factor * V[next_state])
+    return action_values
 
-pp = pprint.PrettyPrinter(indent=2)
-env = GridworldEnv()
-
-
-# In[5]:
 
 
 def value_iteration(env, theta=0.0001, discount_factor=1.0):
@@ -38,18 +54,48 @@ def value_iteration(env, theta=0.0001, discount_factor=1.0):
         A tuple (policy, V) of the optimal policy and the optimal value function.        
     """
     
-
+    # Start with a "random" (all 0) value function, 
+    # except for V(terminal) which must be zero.
     V = np.zeros(env.nS)
-    policy = np.zeros([env.nS, env.nA])
     
-    # Implement!
+    iter = 0
+    while True:
+        iter += 1
+        delta = 0
+        # For each state, perform a "full backup"
+        for s in range(env.nS):
+            v = V[s]
+            """
+            action_values = np.zeros(env.nA)
+            # Look at the possible next actions
+            for a in range(env.nA):
+                # For each action, look at the possible next states and
+                # calculate the expected value
+                for prob, next_state, reward, done in env.P[s][a]:
+                    action_values[a] += prob * (reward + discount_factor * V[next_state])
+            """
+            action_values = lookahead(env, s, V, discount_factor)
+            V[s] = max(action_values)
+            delta = max(delta, np.abs(V[s] - v))
+        if delta < theta:
+            print("Number of iterations= ",iter)
+            break
+    
+    # Output a deterministic policy, such that 
+    # Policy(s) = argmax(action, Sum(prob * (reward + discount_factor * V[next_state])))
+    policy = np.zeros([env.nS, env.nA])
+    for s in range(env.nS):
+        # One step lookahead to find the best action for this state
+        best_action = np.argmax(lookahead(env,s, V, discount_factor))
+        # Always take the best action
+        policy[s, best_action] = 1.0
+    
     return policy, V
 
 
-# In[6]:
-
 
 policy, v = value_iteration(env)
+
 
 print("Policy Probability Distribution:")
 print(policy)
@@ -68,10 +114,10 @@ print(v.reshape(env.shape))
 print("")
 
 
-# In[7]:
 
 
 # Test the value function
-expected_v = np.array([ 0, -1, -2, -3, -1, -2, -3, -2, -2, -3, -2, -1, -3, -2, -1,  0])
-np.testing.assert_array_almost_equal(v, expected_v, decimal=2)
+# GridworldEnv
+#expected_v = np.array([ 0, -1, -2, -3, -1, -2, -3, -2, -2, -3, -2, -1, -3, -2, -1,  0])
+#np.testing.assert_array_almost_equal(v, expected_v, decimal=2)
 
