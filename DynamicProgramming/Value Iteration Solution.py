cd9004ddd655e11bf1353f5ff309980c97765f48
AA <73504371+learningparity@users.noreply.github.com>
2020-11-03 03:54:17 +0100

    Finally solved the value iteration

--- DynamicProgramming/Value Iteration Solution.py
+++ DynamicProgramming/Value Iteration Solution.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 import numpy as np
-import pprint
+#import pprint
 import sys
 if "../" not in sys.path:
   sys.path.append("../") 
-from lib.envs.gridworld import GridworldEnv
 
+#from lib.envs.gridworld import GridworldEnv
+#env = GridworldEnv()
 
-pp = pprint.PrettyPrinter(indent=2)
-env = GridworldEnv()
-
+from lib.envs.cliff_walking import CliffWalkingEnv
+env = CliffWalkingEnv()
+#pp = pprint.PrettyPrinter(indent=2)
 
 def lookahead(env, state, V, discount_factor):
     """
@@ -25,9 +26,14 @@ def lookahead(env, state, V, discount_factor):
     """
     action_values = np.zeros(env.nA)
     for a in range(env.nA):
-        # [(prob, next_state, reward, done)] = env.P[state][a]
         for prob, next_state, reward, done in env.P[state][a]:
-            action_values[a] += prob * (reward + discount_factor * V[next_state])
+            #action_values[a] += prob * (reward + discount_factor * V[next_state])
+            # How to deal with reaching terminal states?
+            # Is this correct?
+            if done:
+                action_values[a] += prob * reward
+            else:
+                action_values[a] += prob * (reward + discount_factor * V[next_state])
     return action_values
 
 
@@ -51,7 +57,6 @@ def value_iteration(env, theta=0.0001, discount_factor=1.0):
     # Start with a "random" (all 0) value function, 
     # except for V(terminal) which must be zero.
     V = np.zeros(env.nS)
-    policy = np.zeros([env.nS, env.nA])
     
     iter = 0
     while True:
@@ -59,98 +64,38 @@ def value_iteration(env, theta=0.0001, discount_factor=1.0):
         delta = 0
         # For each state, perform a "full backup"
         for s in range(env.nS):
-            
-            best_action_value = 0
+            v = V[s]
+            """
+            action_values = np.zeros(env.nA)
             # Look at the possible next actions
             for a in range(env.nA):
                 # For each action, look at the possible next states and
                 # calculate the expected value
-                [(prob, next_state, reward, done)] = env.P[s][a]
-                current_value = prob * (reward + discount_factor * V[next_state])
-                best_action_value = max(current_value, best_action_value)
-            delta = max(delta, np.abs(best_action_value - V[s]))
-            V[s] = best_action_value
+                for prob, next_state, reward, done in env.P[s][a]:
+                    action_values[a] += prob * (reward + discount_factor * V[next_state])
+            """
+            action_values = lookahead(env, s, V, discount_factor)
+            V[s] = max(action_values)
+            delta = max(delta, np.abs(V[s] - v))
         if delta < theta:
             print("Number of iterations= ",iter)
             break
     
     # Output a deterministic policy, such that 
     # Policy(s) = argmax(action, Sum(prob * (reward + discount_factor * V[next_state])))
-    for s in range(env.nS):
-        # One step lookahead to find the best action for this state
-        best_action = lookahead(env,s, V, discount_factor)
-        # Always take the best action
-        policy[s, best_action] = 1.0
-    
-    return policy, V
-
-
-def value_iteration_denny(env, theta=0.0001, discount_factor=1.0):
-    """
-    Value Iteration Algorithm.
-    
-    Args:
-        env: OpenAI env. env.P represents the transition probabilities of the environment.
-            env.P[s][a] is a list of transition tuples (prob, next_state, reward, done).
-            env.nS is a number of states in the environment. 
-            env.nA is a number of actions in the environment.
-        theta: We stop evaluation once our value function change is less than theta for all states.
-        discount_factor: Gamma discount factor.
-        
-    Returns:
-        A tuple (policy, V) of the optimal policy and the optimal value function.
-    """
-    
-    V = np.zeros(env.nS)
-    while True:
-        # Stopping condition
-        delta = 0
-        # Update each state...
-        for s in range(env.nS):
-            # Do a one-step lookahead to find the best action
-            A = lookahead(env, s, V, discount_factor)
-            best_action_value = np.max(A)
-            # Calculate delta across all states seen so far
-            delta = max(delta, np.abs(best_action_value - V[s]))
-            # Update the value function. Ref: Sutton book eq. 4.10. 
-            V[s] = best_action_value        
-        # Check if we can stop 
-        if delta < theta:
-            break
-    
-    # Create a deterministic policy using the optimal value function
     policy = np.zeros([env.nS, env.nA])
     for s in range(env.nS):
         # One step lookahead to find the best action for this state
-        A = lookahead(env, s, V, discount_factor)
-        best_action = np.argmax(A)
+        best_action = np.argmax(lookahead(env,s, V, discount_factor))
         # Always take the best action
         policy[s, best_action] = 1.0
     
     return policy, V
+
 
 
 policy, v = value_iteration(env)
 
-print("Policy Probability Distribution:")
-print(policy)
-print("")
-
-print("Reshaped Grid Policy (0=up, 1=right, 2=down, 3=left):")
-print(np.reshape(np.argmax(policy, axis=1), env.shape))
-print("")
-
-print("Value Function:")
-print(v)
-print("")
-
-print("Reshaped Grid Value Function:")
-print(v.reshape(env.shape))
-print("")
-
-
-policy, v = value_iteration_denny(env)
-#policy, v = value_iteration(env)
 
 print("Policy Probability Distribution:")
 print(policy)
@@ -167,9 +112,12 @@ print("")
 print("Reshaped Grid Value Function:")
 print(v.reshape(env.shape))
 print("")
+
+
 
 
 # Test the value function
-expected_v = np.array([ 0, -1, -2, -3, -1, -2, -3, -2, -2, -3, -2, -1, -3, -2, -1,  0])
-np.testing.assert_array_almost_equal(v, expected_v, decimal=2)
+# GridworldEnv
+#expected_v = np.array([ 0, -1, -2, -3, -1, -2, -3, -2, -2, -3, -2, -1, -3, -2, -1,  0])
+#np.testing.assert_array_almost_equal(v, expected_v, decimal=2)
 
